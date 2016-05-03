7fa788419a9c7cabf99f85a69a795cafd056c8cf
Henry Borchers <henryborchers@yahoo.com>
2016-05-03 17:06:24 -0500

    Added docstrings

--- MigrationTools/MetadataReader.py
+++ MigrationTools/MetadataReader.py
@@ -6,8 +6,9 @@ import re
 
 Items = namedtuple("collection", ['name', 'files'])
 
-class CDM_md_base:
+class _CDM_md_base:
     def __init__(self, tsv_file):
+
         self.records = self.load_data(tsv_file)
 
     def __iter__(self):
@@ -23,7 +24,12 @@ class CDM_md_base:
         return False
 
     def get_record(self, contentDM_number):
-        pass
+        """
+        Get a single record from a ContentDM number
+
+        :param contentDM_number: The ContentDM number
+        :returns:   dict -- Single item record
+        """
         for record in self.records:
             if record['CONTENTdm number'] == str(contentDM_number):
                 return record
@@ -33,7 +39,7 @@ class CDM_md_base:
     @staticmethod
     def load_data(tsv_file):
         records = []
-        with open(tsv_file) as f:
+        with open(tsv_file, 'r', encoding="utf-8") as f:
 
             data = csv.DictReader(f, delimiter='\t')
             for row in data:
@@ -41,16 +47,31 @@ class CDM_md_base:
         return records
 
 
-class CDMMetadata(CDM_md_base):
+class CDMMetadata(_CDM_md_base):
+    """
+    Use for reading the data found in an ContentDM exported TSV file.
+
+      Args:
+        tsv_file: the file name of the tsv file that contains ContentDM Metadata
+
+    """
     def has_column(self, metadata_type):
+        """
+        Used for find out if the TSV file imported includes a certain metadata field.
+
+        :param metadata_type: A type of metadata such as a "filename"
+        :returns: bool -- if it exists in the metadata tsv or not
+
+        """
         try:
-            self.records[0][metadata_type]
+            assert self.records[0][metadata_type] is not None
             return True
         except KeyError:
             return False
 
     @property
     def columns(self):
+        """Return all the column headers found in the tsv files"""
         keys = sorted(self.records[0].keys())
         return keys
 

228f0d259004ebd87da8d70eb20beb6153a4122a
Henry Borchers <henryborchers@yahoo.com>
2016-06-03 13:38:27 -0500

    added PickeledFinder and CachedFinder

--- MigrationTools/Finders.py
+++ MigrationTools/Finders.py
@@ -1,4 +1,6 @@
 import os
+import re
+import pickle
 
 
 def find_file_locally(filename, path):
@@ -6,3 +8,57 @@ def find_file_locally(filename, path):
         for file_ in files:
             if file_ == filename:
                 yield os.path.join(root, file_)
+
+
+class PickeledFinder:
+    def __init__(self, cache_file=None):
+        self._tree = None
+        self._cached_file = cache_file
+
+        if self._cached_file is not None:
+            self.load(self._cached_file)
+
+    def map_path(self, path):
+        self._tree = list(os.walk(path))
+
+    def walk(self):
+        if self._tree is None:
+            raise Exception("No data loaded")
+        return self._tree
+
+    def save(self, filename):
+        with open(filename, "wb") as f:
+            pickle.dump(self._tree, f)
+        pass
+
+    def load(self, filename):
+        with open(filename, "rb") as f:
+            self._tree = pickle.load(f)
+        pass
+
+
+
+class CachedFinder:
+
+    def __init__(self, path):
+        self._tree = []
+        for item in os.walk(path):
+            self._tree.append(item)
+
+    def find_file(self, filename):
+        results = []
+        for root, dirs, files in self._tree:
+            for file_ in files:
+                if file_ == filename:
+                    results.append(os.path.join(root, file_))
+        return results
+
+    def regex_matches(self, regex):
+        pattern = re.compile(regex, re.IGNORECASE)
+        results = []
+
+        for root, dirs, files in self._tree:
+            for file_ in files:
+                if re.match(pattern, file_):
+                    results.append(os.path.join(root, file_))
+        return results

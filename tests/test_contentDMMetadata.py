712bd47e9a433210e977501985ea1ee9609c5821
Henry Borchers <henryborchers@yahoo.com>
2016-08-18 13:49:22 -0500

    not sure

--- tests/test_contentDMMetadata.py
+++ tests/test_contentDMMetadata.py
@@ -1,8 +1,10 @@
 # from unittest import TestCase
+import os
+
 import pytest
 from MigrationTools.MetadataReader import _CDM_md_base
 
-test_file = "tests/test.tsv"
+test_file = os.path.join(os.path.dirname(__file__), "test.tsv")
 test_columns = ("Title", "Creator", "Place of Publication", "Date", "Coverage-Spatial", "Subject", "Keyword", "Type",
                 "Dimensions", "Language", "Source", "Physical Location", "Bibliography",
                 "Scale", "Notes", "Color", "Local Call Number", "Map No. in Bassett Bibliography",
@@ -17,21 +19,5 @@ def CDM_base(request):
     return _CDM_md_base(test_file)
 
 
-def test_get_record(CDM_base):
-    assert CDM_base.get_record(43)['Title'] == "Map with tributaries to Congo River, Mpozo River"
 
-
-def test_iterate(CDM_base):
-    i = iter(CDM_base)
-    assert next(i)['CONTENTdm file name'] == "100.jp2"
-
-
-def test_contains(CDM_base):
-    assert 44 in CDM_base
-
-
-def test_iter_forloop(CDM_base):
-    for i, record in enumerate(CDM_base):
-        assert isinstance(record, dict)
-        assert i < len(CDM_base), "The class is try to pull more records than exists"
 

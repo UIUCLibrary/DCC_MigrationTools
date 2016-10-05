085a1092359f01e345ac987d899811719e16a82c
Henry Borchers <henryborchers@yahoo.com>
2016-10-05 13:07:20 -0500

    new version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ class PyTest(TestCommand):
 
 setup(
     name='MigrationTools',
-    version='0.1.3',
+    version='0.2.0',
     packages=['MigrationTools', 'tests'],
     url='',
     license='',

113fb5b9059ed98e3545eab64a38f8bc6adbbcf0
Henry Borchers <henryborchers@yahoo.com>
2016-10-05 16:31:17 -0500

    added tests

--- tests/test_contentDMMetadata.py
+++ tests/test_contentDMMetadata.py
@@ -2,9 +2,10 @@
 import os
 
 import pytest
-from MigrationTools.MetadataReader import _CDM_md_base
+from MigrationTools.MetadataReader import CDM_Metadata
 
-test_file = os.path.join(os.path.dirname(__file__), "test.tsv")
+test_file_tsv = os.path.join(os.path.dirname(__file__), "test.tsv")
+test_file_xml_ = os.path.join(os.path.dirname(__file__), "export.xml")
 test_columns = ("Title", "Creator", "Place of Publication", "Date", "Coverage-Spatial", "Subject", "Keyword", "Type",
                 "Dimensions", "Language", "Source", "Physical Location", "Bibliography",
                 "Scale", "Notes", "Color", "Local Call Number", "Map No. in Bassett Bibliography",
@@ -13,11 +14,44 @@ test_columns = ("Title", "Creator", "Place of Publication", "Date", "Coverage-Sp
                 "CONTENTdm number", "CONTENTdm file name", "CONTENTdm file path")
 
 
-@pytest.fixture
-def CDM_base(request):
-    """Loads in the contentdm metadata tsv file and creates a _CDM_md_base object"""
-    return _CDM_md_base(test_file)
+# @pytest.fixture
+# def CDM_base(request):
+#     """Loads in the contentdm metadata tsv file and creates a _CDM_md_base object"""
+#     return _CDM_md_base(test_file)
 
 
+@pytest.fixture()
+def CDMdata_tsv():
+    return CDM_Metadata(test_file_tsv)
 
 
+def test_tsv_fields(CDMdata_tsv):
+    for field in CDMdata_tsv.fields:
+        assert field in test_columns
+
+
+def test_tsv_doesnt_have_field(CDMdata_tsv):
+    assert CDMdata_tsv.has_field("spam") is False
+
+
+def test_tsv_len(CDMdata_tsv):
+    assert len(CDMdata_tsv) == 26
+
+@pytest.fixture()
+def CDMdata_xml():
+    return CDM_Metadata(test_file_xml_)
+
+
+def test_xml_fields(CDMdata_xml):
+    xml_fields = ['isPartOf', 'description', 'fullResolution', 'contributor', 'cdmoclc', 'cdmid', 'publisher', 'title', 'creator', 'viewerURL', 'subject', 'unmapped', 'cdmcreated', 'structure', 'thumbnailURL', 'rights', 'format', 'type', 'source', 'cdmfile', 'date', 'cdmpath', 'cdmmodified', 'cdmaccess']
+    fields = CDMdata_xml.fields
+    for field in fields:
+        assert field in xml_fields
+
+
+def test_xml_doesnt_have_field(CDMdata_xml):
+    assert CDMdata_xml.has_field("spam") is False
+
+
+def test_xml_len(CDMdata_xml):
+    assert len(CDMdata_xml) == 5

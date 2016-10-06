4440a4c88065a6e801225f35496b21012f5bbadc
Henry Borchers <henryborchers@yahoo.com>
2016-10-06 16:54:02 -0500

    added group_id for element to test for

--- tests/test_contentDMMetadata.py
+++ tests/test_contentDMMetadata.py
@@ -4,9 +4,11 @@ import os
 import pytest
 from MigrationTools.MetadataReader import CDM_Metadata
 
-test_file_tsv = os.path.join(os.path.dirname(__file__), "test.tsv")
-test_file_xml_ = os.path.join(os.path.dirname(__file__), "export.xml")
-test_columns = ("Title", "Creator", "Place of Publication", "Date", "Coverage-Spatial", "Subject", "Keyword", "Type",
+test_file1_tsv = os.path.join(os.path.dirname(__file__), "test.tsv")
+test_file_xml = os.path.join(os.path.dirname(__file__), "export.xml")
+test_file_tsv = os.path.join(os.path.dirname(__file__), "export.tsv")
+test_columns = ("group_id", "Title", "Creator", "Place of Publication", "Date", "Coverage-Spatial", "Subject",
+                "Keyword", "Type",
                 "Dimensions", "Language", "Source", "Physical Location", "Bibliography",
                 "Scale", "Notes", "Color", "Local Call Number", "Map No. in Bassett Bibliography",
                 "File Name", "Format", "Technique", "JPEG2000 URL", "Rights", "Collection",
@@ -22,7 +24,7 @@ test_columns = ("Title", "Creator", "Place of Publication", "Date", "Coverage-Sp
 
 @pytest.fixture()
 def CDMdata_tsv():
-    return CDM_Metadata(test_file_tsv)
+    return CDM_Metadata(test_file1_tsv)
 
 
 def test_tsv_fields(CDMdata_tsv):
@@ -39,11 +41,37 @@ def test_tsv_len(CDMdata_tsv):
 
 @pytest.fixture()
 def CDMdata_xml():
-    return CDM_Metadata(test_file_xml_)
+    return CDM_Metadata(test_file_xml)
 
 
 def test_xml_fields(CDMdata_xml):
-    xml_fields = ['isPartOf', 'description', 'fullResolution', 'contributor', 'cdmoclc', 'cdmid', 'publisher', 'title', 'creator', 'viewerURL', 'subject', 'unmapped', 'cdmcreated', 'structure', 'thumbnailURL', 'rights', 'format', 'type', 'source', 'cdmfile', 'date', 'cdmpath', 'cdmmodified', 'cdmaccess']
+    xml_fields = ['isPartOf',
+                  'group_id',
+                  'pageptr',
+                  'pagetitle',
+                  'description',
+                  'fullResolution',
+                  'contributor',
+                  'cdmoclc',
+                  'cdmid',
+                  'publisher',
+                  'title',
+                  'creator',
+                  'viewerURL',
+                  'subject',
+                  'unmapped',
+                  'cdmcreated',
+                  'structure',
+                  'thumbnailURL',
+                  'rights',
+                  'format',
+                  'type',
+                  'source',
+                  'cdmfile',
+                  'date',
+                  'cdmpath',
+                  'cdmmodified',
+                  'cdmaccess']
     fields = CDMdata_xml.fields
     for field in fields:
         assert field in xml_fields
@@ -55,3 +83,68 @@ def test_xml_doesnt_have_field(CDMdata_xml):
 
 def test_xml_len(CDMdata_xml):
     assert len(CDMdata_xml) == 5
+
+
+@pytest.fixture()
+def CDMdata_joined():
+    return CDM_Metadata(test_file_xml, test_file_tsv)
+
+def test_joined_fields(CDMdata_joined):
+
+    joined_fields = ['isPartOf',
+                     'group_id',
+                     'pagetitle',
+                     'pageptr',
+                     'description', 
+                     'fullResolution', 
+                     'contributor', 
+                     'cdmoclc',
+                     'cdmid',
+                     'publisher',
+                     'title',
+                     'creator',
+                     'viewerURL',
+                     'subject',
+                     'unmapped',
+                     'cdmcreated',
+                     'structure',
+                     'thumbnailURL',
+                     'rights',
+                     'format',
+                     'type',
+                     'source',
+                     'cdmfile',
+                     'date',
+                     'cdmpath',
+                     'cdmmodified',
+                     'cdmaccess',
+                     'Identifier',
+                     'Type',
+                     'Title',
+                     'Poster Text',
+                     'Artist',
+                     'Printer',
+                     'Description',
+                     'Subject',
+                     'Date',
+                     'Dimensions',
+                     'Sponsor',
+                     'Location - Room',
+                     'Location - Folder',
+                     'Location - RS',
+                     'Source',
+                     'Rights',
+                     'Collection Title',
+                     'Creating Unit',
+                     'Scanning Date',
+                     'Format',
+                     'OCLC number',
+                     'Date created',
+                     'Date modified',
+                     'Reference URL',
+                     'CONTENTdm number',
+                     'CONTENTdm file name',
+                     'CONTENTdm file path']
+    fields = CDMdata_joined.fields
+    for field in fields:
+        assert field in joined_fields
